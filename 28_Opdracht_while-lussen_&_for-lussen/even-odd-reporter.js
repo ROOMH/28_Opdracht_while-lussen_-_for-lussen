435449fa28c449b00ca7c97d14f9f5fe966a6b07
Romy <80348985+ROOMH@users.noreply.github.com>
2021-07-20 12:45:02 +0200

    Add files via upload

---
+++ 28_Opdracht_while-lussen_&_for-lussen/even-odd-reporter.js
@@ -0,0 +1,30 @@
+// Even Odd Reporter
+
+/*for (let i = 0; i <= 20; i++) {
+  console.log(`Is ${i} even or odd?`);
+} */
+
+
+// Tafels van vermeningvuldiging
+
+/* (let e = 9; e * 20; e++) {
+  console.log(" Hoeveel is ${e} keer");
+} 
+document.write("You are now over 20"); */
+
+
+// The grade assigner 
+
+/* function assignGrade(score) {
+  if (score > 90) {
+    return "A";
+  } else if (score > 80) {
+    return "B";
+  } else if (score > 70) {
+    return "C";
+  } else if (score > 65) {
+    return "D";
+  } else {
+    return "E";
+  }
+} */
